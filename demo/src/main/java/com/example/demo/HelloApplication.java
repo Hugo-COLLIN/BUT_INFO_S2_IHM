3034388e9765712ea3488e6bff4370ba611f7c48
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-05-12 16:07:52 +0200

    répertoire test

---
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 320, 240);
+
+        Button b = new Button();
+        root.getChildren().add(b);
+        
+
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
