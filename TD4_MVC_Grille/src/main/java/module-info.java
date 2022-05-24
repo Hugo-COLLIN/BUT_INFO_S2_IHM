71df8504be7cebd85ae0ea8113424bb6e93c69ed
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-05-24 16:44:24 +0200

    del

--- TD4_MVC_Grille/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.td4_mvc_grille {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.td4_mvc_grille to javafx.fxml;
-    exports com.example.td4_mvc_grille;
-}
